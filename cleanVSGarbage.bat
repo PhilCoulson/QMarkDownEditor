a0ce8f31672566d47de01b4fc0fc880d9aa445b1
PhilCoulson <1435073382@qq.com>
2017-09-29 18:44:36 +0800

    清理.IPCH垃圾

--- cleanVSGarbage.bat
+++ cleanVSGarbage.bat
@@ -1,6 +1,6 @@
 @echo 清理 VS 垃圾中。。。
 
-Del *.VC.db; *.pdb; *.obj; *.ilk; *.exe; *.log; *.tlog; /s
+Del *.VC.db; *.pdb; *.obj; *.ilk; *.exe; *.log; *.tlog; *.ipch /s
 rd markdowneditor.tlog  /s 
 rd .vs  /s 
 @echo 清理完毕！
