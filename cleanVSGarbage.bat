f5ace8b6e0f8c75e2095c25205ca7ddf02cfc657
PhilCoulson <1435073382@qq.com>
2017-09-29 18:51:33 +0800

    清理其它垃圾

--- cleanVSGarbage.bat
+++ cleanVSGarbage.bat
@@ -1,6 +1,12 @@
 @echo 清理 VS 垃圾中。。。
-
+#/q 不确认 /s 所有子目录
 Del *.VC.db; *.pdb; *.obj; *.ilk; *.exe; *.log; *.tlog; *.ipch /s
 rd markdowneditor.tlog  /s 
-rd .vs  /s 
+rd .vs  /q /s 
+rd x64  /q /s 
+rd GeneratedFiles /q /s 
+rd doc /q /s 
+rd debug /q /s 
+rd release /q /s 
+rd src /q /s 
 @echo 清理完毕！
